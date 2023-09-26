5486e4e21ee7dfaf44bf2e496a4c876c4d99a6fb
jbytecode <mhsatman@gmail.com>
2023-09-26 13:02:35 +0300

    float <-> bit conversations

--- byteworks.py
+++ byteworks.py
@@ -1,4 +1,5 @@
-import ctypes 
+import ctypes
+
 
 def float_to_bits(f: float) -> list:
     cf = ctypes.c_float(f)
@@ -6,12 +7,38 @@ def float_to_bits(f: float) -> list:
     bts = [(cu.value >> i) & 1 for i in range(32)]
     return bts
 
-def bits_to_float(b: list) -> float: 
+
+def bits_to_float(b: list) -> float:
     uval = 0
+
     for i in range(32):
-        uval = uval + b[i] * (2 ** i)
+        uval = uval + b[i] * (2**i)
+
     cu = ctypes.c_uint32(uval)
     cf = ctypes.c_float.from_address(ctypes.addressof(cu))
     return cf.value
 
 
+def floats_to_bits(fs: list) -> list:
+    bitlist = []
+
+    for f in fs:
+        bitlist = bitlist + float_to_bits(f)
+
+    return bitlist
+
+
+def bits_to_floats(b: list) -> list:
+    bitsize = len(b)
+    floatssize = int(bitsize / 32)
+    floatvector = [0.0] * floatssize
+    index = 0
+    findex = 0
+
+    while index + 32 <= bitsize:
+        part = b[index : (index + 32)]
+        floatvector[findex] = bits_to_float(part)
+        index = index + 32
+        findex += 1
+
+    return floatvector
--- tests/test_byteworks.py
+++ tests/test_byteworks.py
@@ -4,13 +4,49 @@ def approx(a, b, eps = 0.00001):
     return abs(a-b) <= eps
 
 def test_float2bits():
-    exp = [1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0,
-            0, 1, 0]
+    exp = [
+        1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 
+        0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 
+        0, 0, 0, 0, 0, 0, 1, 0]
     bits = byteworks.float_to_bits(3.14159265)
     assert exp == bits
 
 def test_bits2float():
-    bts = [1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0,
-            0, 1, 0]
+    bts = [
+        1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 
+        0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 
+        0, 0, 0, 0, 0, 0, 1, 0]
     fval = byteworks.bits_to_float(bts)
     assert approx(fval, 3.14159265)
+
+def test_bits2floats():
+    bts = [
+            1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 
+            1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 
+            1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 
+            1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 
+            0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 
+            0, 0, 0, 0, 0, 0, 0, 1, 0
+        ]
+    fvals = byteworks.bits_to_floats(bts)
+    assert len(fvals) == 2
+    assert approx(fvals[0], 3.14159265)
+    assert fvals[0] == fvals[1]
+
+
+def test_floats2bits():
+    a = 4.5
+    b = 20.9
+    c = 2.71828
+    fs = [a, b, c]
+    bits = byteworks.floats_to_bits(fs)
+    
+    assert len(bits) == 32 * len(fs)
+
+    finv = byteworks.bits_to_floats(bits)
+
+    assert len(finv) == len(fs)
+    assert approx(finv[0], a)
+    assert approx(finv[1], b)
+    assert approx(finv[2], c)
+
