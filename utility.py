30fafe8a8fde8deb07da08a899408cb28726b08c
mhsatman@gmail.com <mhsatman@gmail.com>
2023-09-26 20:51:06 +0300

    refactor utility

--- utility.py
+++ utility.py
@@ -1,2 +1,9 @@
 def approx(a, b, eps = 0.00001):
     return abs(a-b) <= eps
+
+def f(xs: list[float]) -> float:
+    return abs(xs[0] - 3.14159265) + abs(xs[1] - 2.71828)
+
+def g(xs: list[float]) -> float:
+    return abs(xs[0] - 7.0) + abs(xs[1] - 77.0) + abs(xs[2] - 777.0)
+
